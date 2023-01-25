7714a673d66ad4f2e5418c5b20b7a6f421dde66c
Luis Aragon <luis.aragon8984@est.uca.edu.ni>
2023-01-25 11:50:41 -0600

    Agregando Proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace CompaniaPrueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtIdProducto = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.companiaDataSet = new CompaniaPrueba.CompaniaDataSet();
+            this.productoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productoTableAdapter = new CompaniaPrueba.CompaniaDataSetTableAdapters.ProductoTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descripcionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codigoProductoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precioDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companiaDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(158, 121);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(66, 13);
+            this.lblDescripcion.TabIndex = 0;
+            this.lblDescripcion.Text = "Descripcion:";
+            this.lblDescripcion.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(230, 114);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(270, 20);
+            this.txtDescripcion.TabIndex = 1;
+            this.txtDescripcion.TextChanged += new System.EventHandler(this.txtDescripcion_TextChanged);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(230, 79);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(111, 20);
+            this.txtCodigo.TabIndex = 2;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(181, 86);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigo.TabIndex = 3;
+            this.lblCodigo.Text = "Codigo:";
+            this.lblCodigo.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(181, 160);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(40, 13);
+            this.lblPrecio.TabIndex = 4;
+            this.lblPrecio.Text = "Precio:";
+            this.lblPrecio.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(230, 153);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(111, 20);
+            this.txtPrecio.TabIndex = 5;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(334, 25);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(100, 13);
+            this.lblTitulo.TabIndex = 6;
+            this.lblTitulo.Text = "Catalogo Productos";
+            this.lblTitulo.Click += new System.EventHandler(this.label1_Click_3);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(160, 413);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 7;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(241, 413);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 8;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(322, 412);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 9;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(403, 413);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 10;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(484, 412);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(565, 412);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 12;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(157, 51);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(64, 13);
+            this.lblId.TabIndex = 13;
+            this.lblId.Text = "ID Producto";
+            this.lblId.Click += new System.EventHandler(this.label1_Click_4);
+            // 
+            // txtIdProducto
+            // 
+            this.txtIdProducto.Location = new System.Drawing.Point(230, 44);
+            this.txtIdProducto.Name = "txtIdProducto";
+            this.txtIdProducto.ReadOnly = true;
+            this.txtIdProducto.Size = new System.Drawing.Size(111, 20);
+            this.txtIdProducto.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.descripcionDataGridViewTextBoxColumn,
+            this.codigoProductoDataGridViewTextBoxColumn,
+            this.precioDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.productoBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(125, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(540, 193);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // companiaDataSet
+            // 
+            this.companiaDataSet.DataSetName = "CompaniaDataSet";
+            this.companiaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productoBindingSource
+            // 
+            this.productoBindingSource.DataMember = "Producto";
+            this.productoBindingSource.DataSource = this.companiaDataSet;
+            // 
+            // productoTableAdapter
+            // 
+            this.productoTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // descripcionDataGridViewTextBoxColumn
+            // 
+            this.descripcionDataGridViewTextBoxColumn.DataPropertyName = "descripcion";
+            this.descripcionDataGridViewTextBoxColumn.HeaderText = "descripcion";
+            this.descripcionDataGridViewTextBoxColumn.Name = "descripcionDataGridViewTextBoxColumn";
+            // 
+            // codigoProductoDataGridViewTextBoxColumn
+            // 
+            this.codigoProductoDataGridViewTextBoxColumn.DataPropertyName = "codigoProducto";
+            this.codigoProductoDataGridViewTextBoxColumn.HeaderText = "codigoProducto";
+            this.codigoProductoDataGridViewTextBoxColumn.Name = "codigoProductoDataGridViewTextBoxColumn";
+            // 
+            // precioDataGridViewTextBoxColumn
+            // 
+            this.precioDataGridViewTextBoxColumn.DataPropertyName = "precio";
+            this.precioDataGridViewTextBoxColumn.HeaderText = "precio";
+            this.precioDataGridViewTextBoxColumn.Name = "precioDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 476);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtIdProducto);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblDescripcion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companiaDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtIdProducto;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private CompaniaDataSet companiaDataSet;
+        private System.Windows.Forms.BindingSource productoBindingSource;
+        private CompaniaDataSetTableAdapters.ProductoTableAdapter productoTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigoProductoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precioDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CompaniaPrueba
+{
+    public partial class Form1 : Form
+    {
+        private int parsedProductoID;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_3(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=192.168.1.115;Initial Catalog=Compania;Persist Security Info=True;User ID=ITdemetech;Password=ITdemetech2019!";
+            SqlConnection sqlCon = new SqlConnection(connectionString);
+            sqlCon.Open();
+            string commandString = "SELECT id, descripcion, precio FROM Compania.dbo.Producto WHERE codigoProducto = '" + txtCodigo.Text + "'";
+            SqlCommand sqlCmd = new SqlCommand(commandString, sqlCon);
+            SqlDataReader read = sqlCmd.ExecuteReader();
+            while (read.Read())
+            {
+                txtDescripcion.Text = read["descripcion"].ToString();
+                txtPrecio.Text = read["precio"].ToString();
+                txtIdProducto.Text = read["id"].ToString();
+            }
+            sqlCon.Close();
+        }
+
+        private void txtDescripcion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+            // Inicializando conneccion.
+            using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.ConnString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("dbo.usp_CrearProducto", connection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                    sqlCommand.Parameters.Add(new SqlParameter("@descripcion", SqlDbType.NVarChar, 255));
+                    sqlCommand.Parameters["@descripcion"].Value = txtDescripcion.Text;
+                    sqlCommand.Parameters.Add(new SqlParameter("@codigoProducto", SqlDbType.NVarChar, 20));
+                    sqlCommand.Parameters["@codigoProducto"].Value = txtCodigo.Text;
+                    sqlCommand.Parameters.Add(new SqlParameter("@precio", SqlDbType.Int));
+                    sqlCommand.Parameters["@precio"].Value = txtPrecio.Text;
+
+                    try
+                    {
+                        connection.Open();
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("El producto no se ha podido guardar");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                        MessageBox.Show("PRODUCTO GUARDADO CORRECTAMENTE");
+                    }
+                }
+            }
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtIdProducto.Clear();
+            txtCodigo.Clear();
+            txtDescripcion.Clear();
+            txtPrecio.Clear();
+            this.parsedProductoID = 0;
+        }
+
+        private void label1_Click_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'companiaDataSet.Producto' table. You can move, or remove it, as needed.
+            this.productoTableAdapter.Fill(this.companiaDataSet.Producto);
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            // Inicializando conneccion.
+            using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.ConnString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("dbo.usp_EliminarProducto", connection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                    sqlCommand.Parameters.Add(new SqlParameter("@IDProducto", SqlDbType.Int));
+                    sqlCommand.Parameters["@IDProducto"].Value = txtIdProducto.Text;
+
+                    try
+                    {
+                        connection.Open();
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("El producto no se ha podido eliminar");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                        MessageBox.Show("PRODUCTO ELIMINADO CORRECTAMENTE");
+                    }
+                }
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+
+            // Inicializando conneccion.
+            using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.ConnString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("dbo.usp_ActualizarProducto", connection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                    sqlCommand.Parameters.Add(new SqlParameter("@IDProducto", SqlDbType.Int));
+                    sqlCommand.Parameters["@IDProducto"].Value = txtIdProducto.Text;
+                    sqlCommand.Parameters.Add(new SqlParameter("@descripcion", SqlDbType.NVarChar, 255));
+                    sqlCommand.Parameters["@descripcion"].Value = txtDescripcion.Text;
+                    sqlCommand.Parameters.Add(new SqlParameter("@codigoProducto", SqlDbType.NVarChar, 20));
+                    sqlCommand.Parameters["@codigoProducto"].Value = txtCodigo.Text;
+                    sqlCommand.Parameters.Add(new SqlParameter("@precio", SqlDbType.Int));
+                    sqlCommand.Parameters["@precio"].Value = txtPrecio.Text;
+
+                    try
+                    {
+                        connection.Open();
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("El producto no se ha podido actualizar");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                        dataGridView1.DataSource = null;
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.Refresh();
+                        dataGridView1.Show();
+                        MessageBox.Show("PRODUCTO ACTUALIZADO CORRECTAMENTE");
+                    }
+                }
+
+            }
+        }
+    }
+}
